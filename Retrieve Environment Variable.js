ed10ceecbbb2479fd1ecf64095dc3ebac0936867
Jatinder Singh <singh.jatinder@yahoo.com>
2024-01-28 22:43:14 -0500

    updated formatting

--- Retrieve Environment Variable.js
+++ Retrieve Environment Variable.js
@@ -1,31 +1,29 @@
 //Retrieve the "Current Value" of an Environment Variable
 var Sdk = window.Sdk || {};
-    Sdk.RetrieveEnvironmentVariableValueRequest = function (definitionSchemaName) {
-        this.DefinitionSchemaName = definitionSchemaName;
-    };
-    Sdk.RetrieveEnvironmentVariableValueRequest.prototype.getMetadata = function () {
-        return {
-            boundParameter: null,
-            parameterTypes: {
-                "DefinitionSchemaName": {
-                    "typeName": "Edm.String",
-                    "structuralProperty": 1
-                }
-            },
-         operationType: 1,
-         operationName: "RetrieveEnvironmentVariableValue"
+Sdk.RetrieveEnvironmentVariableValueRequest = function (definitionSchemaName) {
+    this.DefinitionSchemaName = definitionSchemaName;
+};
+Sdk.RetrieveEnvironmentVariableValueRequest.prototype.getMetadata = function () {
+    return {
+        boundParameter: null,
+        parameterTypes: {
+            "DefinitionSchemaName": {
+                "typeName": "Edm.String",
+                "structuralProperty": 1
+            }
+        },
+        operationType: 1,
+        operationName: "RetrieveEnvironmentVariableValue"
     };
 };
- 
 var retrieveEnvironmentVariableValueRequest = new Sdk.RetrieveEnvironmentVariableValueRequest({EnvironmentVariableSchemaName});
-
 Xrm.WebApi.online.execute(retrieveEnvironmentVariableValueRequest).then(
 function success(result) {
     if (result.ok) {
         result.json().then(
             function (responseBody) {
                 console.log(responseBody.Value);
-                         
+                        
             });
     }
 },
